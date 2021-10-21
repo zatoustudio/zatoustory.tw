4bb1342d2eeaf5163cd40c9b13d6423fe9948e44
miksin <contact@miksin.art>
2021-10-21 17:12:49 +0900

    prettier & colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export interface Palette {
+  light?: string
+  main: string
+  dark?: string
+  contrastText?: string
+}
+
+export const Colors = {
+  Primary: '#9079ad',
+  Secondary: '#eb6ea5',
+  White: '#ffffff',
+} as const
